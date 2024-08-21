3ffd1ef86e49d7594d3ecff58a35186861782e95
David G <himynamesdave@users.noreply.github.com>
2024-08-21 11:28:14 +0100

    updating design of crypto wallet

--- generators/scos/cryptocurrency-wallet.py
+++ generators/scos/cryptocurrency-wallet.py
@@ -25,7 +25,7 @@ namespace=UUID("00abedb4-aa42-466c-9c01-fed23315a9b7")
 example_CryptocurrencyWalletSCO = CryptocurrencyWallet(
                     id="cryptocurrency-wallet--"+ str(uuid.uuid5(namespace, f"1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY")),
                     address="1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY",
-                    exchange_ref="identity--1ee9d44a-c962-59b5-adbf-e47cb3f03b92",
+                    holder_ref="identity--1ee9d44a-c962-59b5-adbf-e47cb3f03b92",
                     )
 
 # Write the objects to the filestore
--- stix2extensions/cryptocurrency_wallet.py
+++ stix2extensions/cryptocurrency_wallet.py
@@ -12,7 +12,7 @@ _type = 'cryptocurrency-wallet'
     ('spec_version', StringProperty(fixed='2.1')),
     ('id', IDProperty(_type, spec_version='2.1')),
     ('address', StringProperty(required=True)),
-    ('exchange_ref', ReferenceProperty(valid_types='identity', spec_version='2.1')),
+    ('holder_ref', ReferenceProperty(valid_types='identity', spec_version='2.1')),
     # ('extensions', ExtensionsProperty(spec_version='2.1'))
 ], extension_name=cryptocurrency_wallet_ExtensionDefinitionSMO.id,id_contrib_props=['address'])
 class CryptocurrencyWallet(object):
