af159e15b8d416fa579903a57b2288677264d71f
Fadl <chaos@efqr.dev>
2024-11-26 15:40:54 +0100

    add revoked property to Weakness object muchdogesec/cwe2stix#5

--- stix2extensions/weakness.py
+++ stix2extensions/weakness.py
@@ -1,9 +1,9 @@
 
 from stix2 import CustomObject
 from stix2.properties import (
-    ExtensionsProperty, ReferenceProperty,
+    ReferenceProperty,
     IDProperty, ListProperty, StringProperty,
-    TimestampProperty, TypeProperty,
+    TimestampProperty, TypeProperty, BooleanProperty,
 )
 from stix2.v21.common import (
     ExternalReference,
@@ -21,6 +21,7 @@ _type = 'weakness'
     ('modified', TimestampProperty(default=lambda: NOW, precision='millisecond', precision_constraint='min')),
     ('name', StringProperty(required=True)),
     ('description', StringProperty()),
+    ('revoked', BooleanProperty(default=lambda: False)),
     ('modes_of_introduction', ListProperty(StringProperty)),
     ('likelihood_of_exploit', ListProperty(StringProperty)),
     ('common_consequences', ListProperty(StringProperty)),
