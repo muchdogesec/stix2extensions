muchdogesec/stix2extensions

commit 0fed76552a2e105af86190022d647c4c863dd011

use static identity for bank where issuer name is unknown #17 (#18)

stix2extensions/tools/creditcard2stix.py
@@ -29,6 +29,24 @@ def get_bin_data(card_number, api_key):
         return None
 
 def create_identity(bin_data):
+    if not issuer['name']:
+        return Identity(
+            type="identity",
+            spec_version="2.1",
+            id="identity--643246fc-9204-5b4b-976d-2e605b355c24",
+            created_by_ref="identity--d287a5a4-facc-5254-9563-9e92e3e729ac",
+            created="2020-01-01T00:00:00.000Z",
+            modified="2020-01-01T00:00:00.000Z",
+            name="Unknown Bank",
+            identity_class="organization",
+            sectors=[
+                "financial-services"
+            ],
+            object_marking_refs=[
+                "marking-definition--94868c89-83c2-464b-929b-a1a8aa3c8487",
+                "marking-definition--d287a5a4-facc-5254-9563-9e92e3e729ac"
+            ]
+        )
     issuer = bin_data['BIN']['issuer']
     country = bin_data['BIN']['country']
     name = f"{issuer['name']} ({country['alpha2']})"

commit 30650e7e7b84ba71361c9c27de9bf0460c373e6e

fix error in creditcard identity introduced in #18

stix2extensions/tools/creditcard2stix.py
@@ -29,6 +29,11 @@ def get_bin_data(card_number, api_key):
         return None
 
 def create_identity(bin_data):
+    issuer = bin_data['BIN']['issuer']
+    country = bin_data['BIN']['country']
+    name = f"{issuer['name']} ({country['alpha2']})"
+    identity_id = f"identity--{str(uuid.uuid5(IDENTITY_NS, name))}"
+
     if not issuer['name']:
         return Identity(
             type="identity",
@@ -47,10 +52,8 @@ def create_identity(bin_data):
                 "marking-definition--d287a5a4-facc-5254-9563-9e92e3e729ac"
             ]
         )
-    issuer = bin_data['BIN']['issuer']
-    country = bin_data['BIN']['country']
-    name = f"{issuer['name']} ({country['alpha2']})"
-    identity_id = f"identity--{str(uuid.uuid5(IDENTITY_NS, name))}"
+
+
     return Identity(
         id=identity_id,
         name=name,