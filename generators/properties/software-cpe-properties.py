4476aeca2c97e8a74f8dc0e346c5d41cf496105d
David G <himynamesdave@users.noreply.github.com>
2025-10-02 15:58:01 +0100

    Update software-cpe-properties.py

--- generators/properties/software-cpe-properties.py
+++ generators/properties/software-cpe-properties.py
@@ -46,6 +46,9 @@ example_SoftwareSCO = Software(
                             "target_hw": "*",
                             "other": "*"
                         },
+                        x_revoked=False,
+                        x_created="2020-01-01T00:00:00.000Z",
+                        x_modified="2020-01-01T00:00:00.000Z",
                         extensions={
                             software_cpe_properties_ExtensionDefinitionSMO.id: {
                                     "extension_type": "toplevel-property-extension"
