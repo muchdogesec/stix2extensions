25f4969a911f1a5e557377dd1145c78d93180c33
Chaos Pjeles <fqrious@gmail.com>
2025-10-05 13:29:17 +0100

    Fix datasource (#35)
    * remove name from data-source
    
    * bump

--- stix2extensions/data_source.py
+++ stix2extensions/data_source.py
@@ -1,22 +1,31 @@
 from stix2 import CustomObservable
 from stix2.properties import (
-    ExtensionsProperty, ReferenceProperty,
-    IDProperty, ListProperty, StringProperty, BooleanProperty,
-    TypeProperty
+    ExtensionsProperty,
+    ReferenceProperty,
+    IDProperty,
+    ListProperty,
+    StringProperty,
+    BooleanProperty,
+    TypeProperty,
 )
 from ._extensions import data_source_ExtensionDefinitionSMO
 
-_type = 'data-source'
-@CustomObservable('data-source', [
-    ('type', TypeProperty(_type, spec_version='2.1')),
-    ('spec_version', StringProperty(fixed='2.1')),
-    ('id', IDProperty(_type, spec_version='2.1')),
-    ('name', StringProperty(required=True)),
-    ('category', StringProperty()),
-    ('product', StringProperty()),
-    ('service', StringProperty()),
-    ('definition', StringProperty()),
-    # ('extensions', ExtensionsProperty(spec_version='2.1'))
-], extension_name=data_source_ExtensionDefinitionSMO.id, id_contrib_props=['name'])
+_type = "data-source"
+
+
+@CustomObservable(
+    "data-source",
+    [
+        ("type", TypeProperty(_type, spec_version="2.1")),
+        ("spec_version", StringProperty(fixed="2.1")),
+        ("id", IDProperty(_type, spec_version="2.1")),
+        ("category", StringProperty()),
+        ("product", StringProperty()),
+        ("service", StringProperty()),
+        ("definition", StringProperty()),
+    ],
+    extension_name=data_source_ExtensionDefinitionSMO.id,
+    id_contrib_props=["category", "product", "service"],
+)
 class DataSource(object):
     pass
